df39cb5dd87d5cd6f9f164e12a01b819210a7a7b
hamed701326 <sadeghi0hamed@gmail.com>
2020-11-01 20:25:47 +0330

    start program

---
+++ src/MainApp.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class MainApp {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        System.out.print("number of ball : ");
+        String number=scanner.next();
+
+    }
+    public int findSolutionToFindWeightestBall(int numberOfBalll){
+        return 0;
+    }
+}
